4c6714c5af57323c952e1492ba2e15692b202034
KillPhill2111 <filipe.fonseca.eng@outlook.com>
2026-08-19 20:46:04 -0300

    11º commit

--- backend/ServicoEstoque/obj/Debug/net10.0/ServicoEstoque.AssemblyInfo.cs
+++ backend/ServicoEstoque/obj/Debug/net10.0/ServicoEstoque.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServicoEstoque")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9444f02520ba0f67048669e09297d05f7f7d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aee6eda10c5f203c7a6e2d1bdab6e90d5affdc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServicoEstoque")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServicoEstoque")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/ServicoFaturamento/obj/Debug/net10.0/ServicoFaturamento.AssemblyInfo.cs
+++ backend/ServicoFaturamento/obj/Debug/net10.0/ServicoFaturamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServicoFaturamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9444f02520ba0f67048669e09297d05f7f7d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aee6eda10c5f203c7a6e2d1bdab6e90d5affdc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServicoFaturamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServicoFaturamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
